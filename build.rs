16c55aafaca3c0b83e592c7f32311bf82ca589f8
nahco314 <nahco3_ta@yahoo.co.jp>
2024-10-21 07:48:04 +0900

    fix: fix build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=./deps/add_one/target/release");
+    println!(
+        "cargo:rustc-link-search=native=./deps/add_one/target/{}/release",
+        std::env::var("TARGET").unwrap()
+    );
     println!("cargo:rustc-link-lib=dylib=add_one");
 }
