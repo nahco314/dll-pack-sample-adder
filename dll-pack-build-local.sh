f2ab8533031b49edf7d8ff14fe389450225e9931
nahco314 <nahco3_ta@yahoo.co.jp>
2024-10-21 08:00:44 +0900

    chore: add DYLD_LIBRARY_PATH

--- dll-pack-build-local.sh
+++ dll-pack-build-local.sh
@@ -7,9 +7,11 @@ cd ../../
 
 cargo build --profile release --target ${DLL_PACK_TARGET}
 
+export LD_LIBRARY_PATH=./deps/add_one/target/${DLL_PACK_TARGET}/release/:${LD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=./deps/add_one/target/${DLL_PACK_TARGET}/release/:${DYLD_LIBRARY_PATH}
+
 mkdir ./artifacts/
-LD_LIBRARY_PATH=./deps/add_one/target/${DLL_PACK_TARGET}/release/ \
-    dll-pack-builder local $(cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].name') \
+dll-pack-builder local $(cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].name') \
     $(dll-pack-builder find ${BUILD_OUT_DIR}) \
      ./artifacts/ ${DLL_PACK_TARGET} ${GITHUB_REPOSITORY} ${GITHUB_REF#refs/tags/} \
     --include "deps/**/*"
